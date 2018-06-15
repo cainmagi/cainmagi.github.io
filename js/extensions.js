a02d8edb9a1aa87cd6d84f37b27b3cbf14d9e594
cainmagi <cainmagi@gmail.com>
2018-06-15 15:25:36 -0500

    @ 20180615
    1. add elevate-zoom plugin
    2. add toop-tip plugin
    3. add notes/note20180605sp, playground/20180614_linux_docker and playground/20180614_linux_questions

--- js/extensions.js
+++ js/extensions.js
@@ -18,5 +18,31 @@ $(document).ready(function() {
     setSemanticUIColor(); 
     $('div .overflow').each(function() { 
         $(this).wrapInner('<div class="check" />'); 
-    })
+    });
+    $('.poptext').each(function() { 
+        $(this).wrap('<div style="display:none"></div>');
+    });
+    $('.popsym').each(function() {
+        $(this).addClass('popitem');
+        var refID = $(this).attr('pref');
+        if (typeof refID !== typeof undefined && refID !== false) {
+            $(this).attr('data-tooltip-content', '#' + refID);
+            $(this).removeAttr('pref');
+        }
+        $(this).prepend('<a class="fa fa-question-circle-o image" aria-hidden="true"/>'); 
+    });
+    $('.popitem').tooltipster({
+        theme: 'tooltipster-borderless',
+        trigger: 'click',
+        animation: 'grow',
+        contentCloning: true,
+        maxWidth: 400
+    });
+    
+    /*var productPopUp = $('div .popup');
+    productPopUp.find('.popuptext').autoposition($("body"));  
+    productPopUp.click(function () {
+        var popText = $(this).find('.popuptext');
+        popText.toggleClass('show');
+    });*/
 });
--- js/jquery.images.js
+++ js/jquery.images.js
@@ -1,4 +1,4 @@
-$('body').append('<div class="product-image-overlay"><span class="product-image-overlay-close"></span><img src="" /></div>');
+$('body').append('<div class="product-image-overlay"><span class="product-image-overlay-close"></span><img src="" data-zoom-image=""/></div>');
 var productParent = $('img').parent(':not(span.image.main):not(a)');
 var productImage = productParent.find('img');
 var productOverlay = $('.product-image-overlay');
@@ -7,20 +7,36 @@ productImage.addClass("show_img");
 
 productImage.click(function () {
     var productImageSource = $(this).attr('src');
-    productImage.removeClass("show_img");
+    productImage.removeClass('show_img');
 
     $('#header').addClass('alt');
 
     productOverlayImage.attr('src', productImageSource);
+    if($(window).width()>768){
+        productOverlayImage.attr('data-zoom-image', productImageSource);
+        productOverlayImage.elevateZoom({
+            zoomType:'inner', 
+            cursor: 'crosshair', 
+            scrollZoom : true,
+            responsive : true,
+            zoomWindowFadeIn: 300,
+			zoomWindowFadeOut: 300,
+			lensFadeIn: 300,
+			lensFadeOut: 300
+        });
+    }
 
     productOverlay.fadeIn(300);
     $('body').css('overflow', 'hidden');
 
     $('.product-image-overlay-close').click(function () {
         productOverlay.fadeOut(300);
-        productImage.addClass("show_img");
+        productImage.addClass('show_img');
         $('body').css('overflow', 'auto');
         $('#header').removeClass('alt');
+        $('.zoomContainer').remove();
+        productOverlayImage.removeData('elevateZoom');
+        productOverlayImage.removeData('zoomImage');
     });
 });
 
